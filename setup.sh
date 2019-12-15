61cde677571d1df2745b9f793589f7151dc42e6e
Miguel Legault <miguel.legault@ctrlweb.ca>
2019-12-15 17:26:08 -0500

    Correct repository to use for demo app

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git clone https://github.com/Laradock/laradock.git
-git clone https://github.com/Laradock/laradock.git ../santa-locator
+git clone https://github.com/ctrlwebinc/santa-locator.git ../santa-locator
 cp env-configured laradock/.env
 cd laradock
 docker-compose up -d nginx mysql workspace
