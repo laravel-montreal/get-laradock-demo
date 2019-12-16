7f9b51182a70c65ed4aad6272a7ef4e2d2ce8e70
Miguel Legault <miguel.legault@ctrlweb.ca>
2019-12-16 17:04:34 -0500

    Adjust production script

--- production-setup.sh
+++ production-setup.sh
@@ -2,9 +2,11 @@
 
 git clone https://github.com/ctrlwebinc/santa-locator.git
 cd santa-locator
+chmod -R a+w storage
+cp ../laravel-production-env .env
 git clone https://github.com/Laradock/laradock.git
 cp ../laradock-env-example-production laradock/.env
 rm laradock/docker-compose.yml
-cp ../production-single-droplet-compose.yml laradock/docker-compose
+cp ../production-single-droplet-compose.yml laradock/docker-compose.yml
 cd laradock
 docker-compose up -d nginx mysql workspace
